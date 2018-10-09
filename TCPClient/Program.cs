460c62c4d88c6b0c4695de7931e42f469419ab8d
Mikx <33861768+Mikky0125@users.noreply.github.com>
2018-10-09 20:15:23 +0200

    5

---
+++ TCPClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ToGram / ToOz <value>");
+            var client = new Client(7000);
+                client.Start();
+                Console.ReadKey();
+        }
+    }
+}
